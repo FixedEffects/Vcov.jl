46076dd8d00516444297a3fa0e8a04a412379681
matthieugomez <gomez.matthieu@gmail.com>
2021-04-12 17:17:49 -0700

    special path for refarrays

--- src/covarianceestimators/vcovcluster.jl
+++ src/covarianceestimators/vcovcluster.jl
@@ -6,38 +6,63 @@
 ## 
 ##############################################################################
 
+
 mutable struct GroupedArray{N} <: AbstractArray{UInt32, N}
     refs::Array{UInt32, N}   # refs must be between 0 and n. 0 means missing
     n::Int                   # Number of potential values (= maximum(refs))
 end
-
-size(g::GroupedArray) = size(g.refs)
-@propagate_inbounds getindex(g::GroupedArray, i::Int) = getindex(g.refs, i)
-@propagate_inbounds getindex(g::GroupedArray, I) = getindex(g.refs, I)
+Base.size(g::GroupedArray) = size(g.refs)
+@propagate_inbounds Base.getindex(g::GroupedArray, i::Int) = getindex(g.refs, i)
+@propagate_inbounds Base.getindex(g::GroupedArray, I) = getindex(g.refs, I)
 
 group(xs::GroupedArray) = xs
 
 function group(xs::AbstractArray)
-	refs = Array{UInt32}(undef, size(xs))
-	invpool = Dict{eltype(xs), UInt32}()
-	n = 0
-	z = UInt32(0)
-	@inbounds for i in eachindex(xs)
-		x = xs[i]
-		if x === missing
-			refs[i] = 0
-		else
-			lbl = get(invpool, x, z)
-			if !iszero(lbl)
-				refs[i] = lbl
-			else
-				n += 1
-				refs[i] = n
-				invpool[x] = n
-			end
-		end
-	end
-	return GroupedArray{ndims(xs)}(refs, n)
+    _group(DataAPI.refarray(xs), DataAPI.refpool(xs))
+end
+
+
+function _group(xs, ::Nothing)
+    refs = Array{UInt32}(undef, size(xs))
+    invpool = Dict{eltype(xs), UInt32}()
+    n = UInt32(0)
+    z = UInt32(0)
+    @inbounds for i in eachindex(xs)
+        x = xs[i]
+        if x === missing
+            refs[i] = 0
+        else
+            lbl = get(invpool, x, z)
+            if !iszero(lbl)
+                refs[i] = lbl
+            else
+                n += 1
+                refs[i] = n
+                invpool[x] = n
+            end
+        end
+    end
+    return GroupedArray{ndims(xs)}(refs, n)
+end
+
+function _group(ra, rp)
+    refs = Array{UInt32}(undef, size(ra))
+    fira = firstindex(ra)
+    firp = firstindex(rp)
+    hashes = Array{UInt32}(undef, length(rp))
+    n = 0
+    for i in eachindex(hashes)
+        if rp[i+firp-1] === missing
+            hashes[i] = UInt32(0)
+        else
+            n += 1
+            hashes[i] = n
+        end
+    end
+    @inbounds for i in eachindex(refs)
+        refs[i] = hashes[ra[i+fira-1]-firp+1]
+    end
+    return GroupedArray{ndims(ra)}(refs, n)
 end
 
 function group(args...)
@@ -65,7 +90,7 @@ function factorize!(g::GroupedArray{N}) where {N}
     refs = g.refs
     invpool = Dict{UInt32, UInt32}()
     n = 0
-	z = UInt32(0)
+    z = UInt32(0)
     @inbounds for i in eachindex(refs)
         x = refs[i]
         if !iszero(x)

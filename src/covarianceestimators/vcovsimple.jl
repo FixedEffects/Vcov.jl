8382bd40f773d08a8274143000b1fd0a85a46000
Matthieu Gomez <gomez.matthieu@gmail.com>
2022-01-16 11:02:45 -0500

    Update vcovsimple.jl

--- src/covarianceestimators/vcovsimple.jl
+++ src/covarianceestimators/vcovsimple.jl
@@ -7,7 +7,7 @@ Estimate variance-covariance matrix with a simple estimator.
 """
 simple() = SimpleCovariance()
 
-show(io::IO, ::SimpleCovariance) =
+Base.show(io::IO, ::SimpleCovariance) =
     print(io, "Simple covariance estimator")
 
 function S_hat(x::RegressionModel, ::SimpleCovariance)

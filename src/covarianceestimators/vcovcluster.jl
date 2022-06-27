b626ea6761a9b171c963b089d07ac6b0563dc05e
Matthieu Gomez <gomez.matthieu@gmail.com>
2022-06-27 12:12:42 -0400

    Update vcovcluster.jl

--- src/covarianceestimators/vcovcluster.jl
+++ src/covarianceestimators/vcovcluster.jl
@@ -71,7 +71,7 @@ function nclusters(v::ClusterCovariance)
     NamedTuple{names(v)}(map(x -> x.ngroups, v.clusters))
 end
 
-function df_FStat(x::RegressionModel, v::ClusterCovariance, ::Bool)
+function dof_tstat(x::RegressionModel, v::ClusterCovariance, ::Bool)
     minimum(nclusters(v)) - 1
 end
 

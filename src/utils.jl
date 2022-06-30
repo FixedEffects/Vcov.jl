17d5de79e7f815da2bcfe86fddff1832930c4f07
Zhiyu Fu <fuzhiyu0@gmail.com>
2022-06-30 18:49:50 -0500

    correct a bug for non positive-semidefinite vcov

--- src/utils.jl
+++ src/utils.jl
@@ -2,10 +2,11 @@ function pinvertible(A::Symmetric, tol = eps(real(float(one(eltype(A))))))
     eigval, eigvect = eigen(A)
     small = eigval .<= tol
     if any(small)
-        @warn "estimated covariance matrix of moment conditions not of full rank.
+        @warn "estimated covariance matrix of moment conditions not positive-semidefinite.
+                adjustment from Cameron, Gelbach & Miller (2011, JBES) applied.
                  model tests should be interpreted with caution."
         eigval[small] .= 0
-        return Symmetric(eigvect' * Diagonal(eigval) * eigvect)
+        return Symmetric(eigvect * Diagonal(eigval) * eigvect')
     else
         return A
     end
